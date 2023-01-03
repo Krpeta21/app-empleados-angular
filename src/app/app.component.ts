10a98a0a064910f0f33aae81e418d623d60071f5
IrvingGlt <41974385+Krpeta21@users.noreply.github.com>
2023-01-03 00:32:25 -0600

    Practica Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de empleados';
+  
+  empleados:Empleado[]=[
+    new Empleado("Irving","Lopez","Programador",10000),
+    new Empleado("Jordi","Chazaro","Comunity Manager",6000),
+    new Empleado("Julian","Martinez","Contador",4000),
+    new Empleado("Luis","Enriquez","Fisico",5000)
+  ];
+  cuadroNombre:string ="";
+  cuadroApellido:string ="";
+  cuadroCargo:string ="";
+  cuadroSalario:number =0;
+  agregarEmpleado(){
+      let miEmpleado = new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+      this.empleados.push(miEmpleado);
+    }
+  eliminarEmpleado(){
+    this.empleados.pop();
+  }
+
 }
