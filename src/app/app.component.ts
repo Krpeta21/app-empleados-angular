9af28b991dc11bbb679f6cd502280ae6dc33c338
IrvingGlt <41974385+Krpeta21@users.noreply.github.com>
2023-01-03 22:36:50 -0600

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,13 @@ import { Empleado } from './empleado.model';
 })
 export class AppComponent {
   titulo = 'Listado de empleados';
-  
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+  }
+eliminarEmpleado(){
+  this.empleados.pop();
+}
   empleados:Empleado[]=[
     new Empleado("Irving","Lopez","Programador",10000),
     new Empleado("Jordi","Chazaro","Comunity Manager",6000),
@@ -19,12 +25,7 @@ export class AppComponent {
   cuadroApellido:string ="";
   cuadroCargo:string ="";
   cuadroSalario:number =0;
-  agregarEmpleado(){
-      let miEmpleado = new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
-      this.empleados.push(miEmpleado);
-    }
-  eliminarEmpleado(){
-    this.empleados.pop();
-  }
+  
+  
 
 }
